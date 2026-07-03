3eed95da7f9ca08b0e759f7321e71aa0746edbfb
bharat.s <bharat.s@yalamanchili.in>
2026-07-03 12:56:29 +0530

    update config

--- day-3/main.tf
+++ day-3/main.tf
@@ -6,10 +6,8 @@ resource "aws_instance" "terraform-instance" {
     ami = "ami-0d351f1b760a30161"
     instance_type = "t3.micro"
     availability_zone = "ap-south-1a"
-    key_pair = "mumbai_kops"
+    key_name = "mumbai_kops"
     root_block_device {
         volume_size = 10  
     }
-    security_group = "Terraform-SG"
-
 }
