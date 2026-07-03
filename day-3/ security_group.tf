99243dc1ddbef80e27db2e34fb6d0189551aa0fd
bharat.s <bharat.s@yalamanchili.in>
2026-07-03 12:43:54 +0530

    add day-3

---
+++ day-3/ security_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "mysg" {
+    name = "Terraform-SG"
+    description = "This is generated through terraform"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_range = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_range = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+}
---
+++ day-3/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "terraform-instance" {
+    
+    tags = {
+        env = dev
+    }
+    ami = "ami-0d351f1b760a30161"
+    instance_type = "t3.micro"
+    availability_zone = ap-south-1a
+    key_pair = "mumbai_kops"
+    root_block_device {
+        volume_size = 10  
+    }
+    security_group = "Terraform-SG"
+
+}
