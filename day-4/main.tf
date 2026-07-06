72ec46746872678afb4ea26018c22982f98ce79d
bharat.s <bharat.s@yalamanchili.in>
2026-07-06 16:54:28 +0530

    update SG

--- day-4/main.tf
+++ day-4/main.tf
@@ -16,11 +16,21 @@ resource "aws_instance" "my-instance" {
 }
 
 resource "aws_security_group" "my_sg" {
+
+    name = "terraform SG"
+
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"] 
     }
 
 }
