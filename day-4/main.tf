e0cc728002b2b9a254bd785aabaf030daa77d692
bharat.s <bharat.s@yalamanchili.in>
2026-07-06 16:46:21 +0530

    add sg

--- day-4/main.tf
+++ day-4/main.tf
@@ -1,4 +1,3 @@
-
 resource "aws_instance" "my-instance" {
     provider = "aws.bombay"
     ami = "ami-0d351f1b760a30161"
@@ -9,9 +8,19 @@ resource "aws_instance" "my-instance" {
     key_name = "mumbai_kops"
     availability_zone = "ap-south-1a"
     count = 1
-    security_groups = ""
+    security_groups = "my_sg"
     root_block_device {
         volume_size = 10
     }
    
+}
+
+resource "aws_security_group" "my_sg" {
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = [0.0.0.0/0]
+    }
+
 }
